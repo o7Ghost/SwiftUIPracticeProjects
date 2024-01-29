65f6e9e848a90c278959a80c3e3bc6dae09e5684
Joseph <qjlu@ucsd.edu>
2024-01-28 17:28:58 -0800

    add swift left to delete and show todolist list

--- ToDoListApp/ToDoList/ToDoList/Views/ToDoListView.swift
+++ ToDoListApp/ToDoList/ToDoList/Views/ToDoListView.swift
@@ -5,20 +5,32 @@
 //  Created by Joseph Lu on 11/12/23.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
 
-    private let userId: String
+//    private let userId: String
 
     init(userId: String) {
-        self.userId = userId
+//        self.userId = userId
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
 
     var body: some View {
         NavigationView {
-            VStack {}.navigationTitle("To Do List").toolbar {
+            VStack {
+                List(items) { item in ToDoListItemView(item: item)
+                    .swipeActions { Button {
+                        viewModel.delete(id: item.id)
+                    } label: {
+                        Text("Delete")
+                    }}.tint(.red)
+                }.listStyle(PlainListStyle())
+            }.navigationTitle("To Do List").toolbar {
                 Button {
                     viewModel.showingViewItemView = true
                 } label: {
@@ -34,6 +46,6 @@ struct ToDoListView: View {
 
 struct ToDoListItems_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView(userId: "")
+        ToDoListView(userId: "lBzi8bj0xUgJ2KBEOZ1zqwUT8Yt1")
     }
 }
