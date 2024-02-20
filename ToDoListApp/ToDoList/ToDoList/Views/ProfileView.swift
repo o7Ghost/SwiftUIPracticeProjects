88ef436a6fa6170c5ebf03febcfc68a43243b203
Joseph <qjlu@ucsd.edu>
2024-02-19 20:36:19 -0800

    added launch screen, profile screen and logout funtion

--- ToDoListApp/ToDoList/ToDoList/Views/ProfileView.swift
+++ ToDoListApp/ToDoList/ToDoList/Views/ProfileView.swift
@@ -12,8 +12,48 @@ struct ProfileView: View {
 
     var body: some View {
         NavigationView {
-            VStack {}.navigationTitle("Profile")
+            VStack {
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
+
+            }.navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ").bold()
+                Text(user.name)
+            }.padding()
+
+            HStack {
+                Text("Email: ").bold()
+                Text(user.email)
+            }.padding()
+            HStack {
+                Text("Member Since: ").bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }.padding()
+        }.padding()
+
+        Button("Log out") {
+            viewModel.logout()
+        }.tint(.red).padding()
     }
 }
 
